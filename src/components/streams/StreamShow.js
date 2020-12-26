d8aada4b7bba57c7c477bdcf33ee4626f49be9ac
abdikasa <abdikasa@sheridancollege.ca>
2020-12-25 21:36:43 -0500

    Finished all CRUD functionality and aded Switch tag to the Routes.

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,7 +1,31 @@
+import { render } from "@testing-library/react";
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+class StreamShow extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    const { title, desc } = this.props.stream;
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div>
+        <h1>{title}</h1>
+        <h5>{desc}</h5>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id],
+  };
 };
 
-export default StreamShow;
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
